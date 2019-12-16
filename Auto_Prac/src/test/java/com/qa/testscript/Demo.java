2d62ecebd16f1db16b73e72f8f21ba5c0bcee052
ritunegi04 <ritu.negi04@gnail.com>
2019-12-16 21:06:32 +0530

    ExtentReports change

--- Auto_Prac/src/test/java/com/qa/testscript/Demo.java
+++ Auto_Prac/src/test/java/com/qa/testscript/Demo.java
@@ -9,21 +9,18 @@ public class Demo extends BaseClass
 	@Test
 	public void run()
 	{
-		//test = report.createTest("Login");
 		System.out.println("hi");
 	}
 	
 	@Test
 	public void run2()
 	{
-		//test = report.createTest("Login1");
 		System.out.println("hello");
 	}
 	
 	@Test
 	public void run3()
 	{
-		//test = report.createTest("Login1");
 		System.out.println("World");
 	}
 
