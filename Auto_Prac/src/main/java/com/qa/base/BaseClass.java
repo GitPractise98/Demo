75f6cae532f3fe7a20a393d87f3d71ab68462f99
nitin thalia <testmarketo98@gmail.com>
2019-12-16 14:57:27 +0530

    BaseCLass

--- Auto_Prac/src/main/java/com/qa/base/BaseClass.java
+++ Auto_Prac/src/main/java/com/qa/base/BaseClass.java
@@ -1,7 +1,18 @@
 package com.qa.base;
 
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+
 public class BaseClass
 {
-	
+	@BeforeSuite
+	public void Report_setup()
+	{
+		ExtentReports reports = new ExtentReports(System.getProperty("user.dir")+"/Reports/myreport.html");
+		
+	}
 
 }
